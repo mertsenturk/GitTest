c7446e6113caf52bea08ef0b8e8ed8743c02402a
Mert Şentürk <mertmrt212@gmail.com>
2020-07-30 13:49:05 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Mert Şentürk on 30.07.2020.
+//  Copyright © 2020 Mert Şentürk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
